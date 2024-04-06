4e85c47b6e13a5fec513723eb53796e48f656e06
dayvidwhy <dayvidwhy@gmail.com>
2024-04-06 17:52:24 +1000

    Add 404 page.

---
+++ pages/[...404].vue
@@ -0,0 +1,13 @@
+<template>
+    <UContainer>
+        <h1 class="text-4xl pb-2 text-slate-500">
+            Page not found
+        </h1>
+        <h3 class="text-2xl text-slate-500">
+            Head back <a
+                class="text-cyan-700 hover:underline"
+                href="/"
+            >home</a> or try another page.
+        </h3>
+    </UContainer>
+</template>
